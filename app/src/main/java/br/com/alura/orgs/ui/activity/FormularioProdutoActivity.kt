705b21cb712e581909d9ae2d39283e34fe7b8615
Jose Arlan <josearlan.fearless@gmail.com>
2023-10-17 16:43:07 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,48 @@
+package br.com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    private val dao = ProdutosDao()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configuraBotaoSalvar()
+    }
+
+    private fun configuraBotaoSalvar() {
+        val botaoSalvar = findViewById<Button>(R.id.activity_formulario_produto_salvar)
+        botaoSalvar.setOnClickListener {
+            val produtoCriado = criaProduto()
+            dao.adiciona(produtoCriado)
+            finish()
+        }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = findViewById<EditText>(R.id.activity_formulario_produto_nome)
+        val nome = campoNome.text.toString()
+        val campoDescricao = findViewById<EditText>(R.id.activity_formulario_produto_descricao)
+        val descricao = campoDescricao.text.toString()
+        val campoValor = findViewById<EditText>(R.id.activity_formulario_produto_valor)
+        val valorEmTexto = campoValor.text.toString()
+        val valor = if (valorEmTexto.isEmpty()) {
+            BigDecimal.ZERO
+        } else {
+            BigDecimal(valorEmTexto)
+        }
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            valor = valor
+        )
+    }
+}
