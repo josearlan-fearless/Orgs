2a4c806612009c42eb182788cc5e39b35d802a6b
Jose Arlan <josearlan.fearless@gmail.com>
2023-11-28 16:33:10 -0300

    Adicionando permissoes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.coil)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
